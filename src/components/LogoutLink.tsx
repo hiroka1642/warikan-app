1ccfbf7c1bd69ff677407fe403c47301eb738fde
katan4722 <uver7770422@gmail.com>
2021-09-26 14:51:10 +0900

    ✨ ログアウト機能を実装

--- src/components/LogoutLink.tsx
+++ src/components/LogoutLink.tsx
@@ -1,13 +1,23 @@
 import { LinkOverlay } from "@chakra-ui/react";
+import { client } from "src/libs/supabase";
 
 type Props = {
   children: string;
   icon: any;
 };
 
+const handleLogout=()=>{
+  client.auth.signOut()
+}
+
 export const LogoutComponent: React.VFC<Props> = (props) => {
   return (
-    <LinkOverlay className="flex items-center gap-1">
+    <LinkOverlay
+      className="flex items-center gap-1"
+      onClick={
+        handleLogout
+      }
+    >
       <p>{props.children}</p>
       {props.icon}
     </LinkOverlay>
