d3d937fc3dda5aa5adba8d636cef6b4f10278605
katan4722 <uver7770422@gmail.com>
2021-10-15 11:20:36 +0900

    🐛 table直下にtbodyを生成

--- src/components/OnProject/SettlementListItem.tsx
+++ src/components/OnProject/SettlementListItem.tsx
@@ -3,19 +3,21 @@ type Props = {
   nameid: Element[];
 };
 
-export const SettlementListItem = (props: Props) => {
+export const SettlementListItem: React.VFC<Props> = (props) => {
   return (
     <>
       <table className="text-xl  m-auto">
-        {props.list.map((li: any) => {
-          return (
-            <tr key={li.id}>
-              <td>{li.what}代</td>
-              <td>{props.nameid[li.payfor] || li.payfor}へ</td>
-              <td>{li.money}円</td>
-            </tr>
-          );
-        })}
+        <tbody>
+          {props.list.map((li: any, key) => {
+            return (
+              <tr key={key}>
+                <td>{li.what}代</td>
+                <td>{props.nameid[li.payfor] || li.payfor}へ</td>
+                <td>{li.money}円</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
