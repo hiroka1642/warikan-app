da593b78709e3fd2547b9461ca1cae83b92b5412
katan4722 <uver7770422@gmail.com>
2021-10-16 22:50:55 +0900

    📱 背景画像のレスポンシブ対応

--- src/components/NewProjectPage/index.tsx
+++ src/components/NewProjectPage/index.tsx
@@ -1,5 +1,5 @@
+import { Box } from "@chakra-ui/react";
 import { useState } from "react";
-import { useEffect } from "react";
 import { Projects } from "src/components/NewProjectPage/Projects";
 import { Header } from "../Header";
 import { NewProject } from "./NewProject";
@@ -8,21 +8,27 @@ export const NewProjectPage: React.VFC = () => {
   const [hasNewProject, setNewProject] = useState<boolean>(false);
   const [list, setList] = useState<string[]>([]);
 
-  useEffect(() => {
-    document.body.style.backgroundImage =
-      "url(.//AfterLoginBackgroundImage.jpg)";
-    document.body.style.backgroundSize = "cover";
-    document.body.style.backgroundColor = "rgba(255,255,255,0.3)";
-    document.body.style.backgroundBlendMode = "lighten";
-  }, []);
   return (
     <>
-      <Header setNewProject={setNewProject} />
-      {hasNewProject ? (
-        <NewProject setNewProject={setNewProject} setList={setList} />
-      ) : (
-        <Projects setNewProject={setNewProject} list={list} setList={setList} />
-      )}
+      <Box
+        backgroundImage="url(.//AfterLoginBackgroundImage.jpg)"
+        backgroundSize="cover"
+        backgroundColor="rgba(255,255,255,0.3)"
+        backgroundBlendMode="lighten"
+      >
+        <div className="h-screen">
+          <Header setNewProject={setNewProject} />
+          {hasNewProject ? (
+            <NewProject setNewProject={setNewProject} setList={setList} />
+          ) : (
+            <Projects
+              setNewProject={setNewProject}
+              list={list}
+              setList={setList}
+            />
+          )}
+        </div>
+      </Box>
     </>
   );
 };
--- src/components/Top.tsx
+++ src/components/Top.tsx
@@ -1,25 +1,42 @@
+import { Box } from "@chakra-ui/react";
 import { Auth } from "@supabase/ui";
-import { useEffect } from "react";
 import { client } from "src/libs/supabase";
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
 
-export const Top :React.VFC= () => {
-  useEffect(() => {
-    document.body.style.backgroundImage = "url(.//webBackgroundImage.jpg)";
-    document.body.style.backgroundSize = "cover";
-  }, []);
+export const Top: React.VFC = () => {
+  const breakpoints = createBreakpoints({
+    sm: "320px",
+    md: "600px",
+    lg: "960px",
+    xl: "1200px",
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const theme = extendTheme({ breakpoints });
 
   return (
     <>
-      <h1 className="text-6xl text-center m-20">みんなでわりかん。</h1>
-      <div className="flex justify-center">
-        <div className="sm:w-96">
-          <Auth
-            supabaseClient={client}
-            providers={["github"]}
-            socialColors={true}
-          />
+      <Box
+        backgroundImage={{
+          base: ".//mobileBackgroundImage.jpg",
+          md: ".//webBackgroundImage.jpg",
+        }}
+        backgroundSize="cover"
+      >
+        <div className="h-screen">
+          <div className="flex justify-center flex-col ">
+              <h1 className="text-4xl text-center py-32 sm:text-6xl">みんなでわりかん。</h1>
+            <div>
+              <div className=" max-w-md m-auto p-8">
+                <Auth
+                  supabaseClient={client}
+                  socialColors={true}
+                />
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </Box>
     </>
   );
 };
