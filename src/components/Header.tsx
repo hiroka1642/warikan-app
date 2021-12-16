9ac10622776a435dccafd77d86cc5871195a1612
katan4722 <uver7770422@gmail.com>
2021-12-16 22:57:19 +0900

    💄 メニューバー変更

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,105 +1,52 @@
-import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 import { useRouter } from "next/dist/client/router";
-import { useState } from "react";
 import { ListIcon, LogoutIcon, NewList } from "./Atom/Icon";
 import { LogoutComponent } from "./Atom/LogoutLink";
+import { Menu } from "./Menu";
 
 export const Header: React.VFC = () => {
-  const [isOpenMenu, setOpenMenu] = useState<boolean>(false);
   const router = useRouter();
-
-  const handleMenuClose = () => {
-    setOpenMenu(false);
-  };
-  const handleMenuOpem = () => {
-    setOpenMenu(true);
-  };
 
   const handleRouter = (url: string) => {
     router.push(url);
   };
 
-  //？？ハンバーガーメニューの作りかたはこれでいい？
-  //？？横から飛び出すようなアニメーションをつけたい
   return (
     <>
-      {isOpenMenu ? (
-        <div>
-          <header className=" sm:flex-row flex flex-col justify-between z-10 bg-white sm:text-blue-800 bg-opacity-60 sm:bg-opacity-30  sm:p-12 w-36 h-full sm:h-auto sm:w-screen fixed top-0 ">
-            <div className="flex sm:flex-row flex-col sm:gap-6 sm:ml-2  sm:w-max">
-              <button className="text-left p-1 sm:invisible">
-                <CloseIcon
-                  width="40px"
-                  height="40px"
-                  onClick={handleMenuClose}
-                />
-              </button>
-              <button
-                className="flex h-20 sm:h-auto items-center"
-                // eslint-disable-next-line arrow-body-style
-                onClick={() => handleRouter("/NewProject")}
-              >
-                <ListIcon color={"rgba(30, 64, 175)"} />
-                チーム一覧
-              </button>
-              <button
-                className="flex h-20 sm:h-auto items-center"
-                // eslint-disable-next-line arrow-body-style
-                onClick={() => handleRouter("/Project.page")}
-              >
-                <NewList color={"rgba(30, 64, 175)"} />
-                新規チーム作成
-              </button>
-            </div>
-            <div className=" flex h-24 sm:h-auto items-center">
-              <LogoutComponent
-                icon={<LogoutIcon color={"rgba(30, 64, 175)"} />}
-              >
-                ログアウト
-              </LogoutComponent>
-            </div>
-          </header>
-        </div>
-      ) : (
-        <div>
-          <button className="sm:invisible top-0 fixed" onClick={handleMenuOpem}>
-            <HamburgerIcon width="45px" height="45px" />
-          </button>
+      <div className="sm:invisible visible ">
+        <Menu />
+      </div>
 
-          <div className=" sm:visible invisible ">
-            <header
-              className=" sm:flex-row flex  justify-between bg-white
+      <div className=" sm:visible invisible ">
+        <header
+          className=" sm:flex-row flex  justify-between bg-white
                 text-blue-800 bg-opacity-30  p-12  h-auto w-screen fixed top-0"
+        >
+          <div className="flex flex-row  gap-6 ml-2  w-max">
+            <button
+              className="flex h-auto items-center"
+              // eslint-disable-next-line arrow-body-style
+              onClick={() => handleRouter("/ProjectList.page")}
             >
-              <div className="flex flex-row  gap-6 ml-2  w-max">
-                <button
-                  className="flex h-auto items-center"
-                  // eslint-disable-next-line arrow-body-style
-                  onClick={() => handleRouter("/ProjectList.page")}
-                >
-                  <ListIcon color={"rgba(30, 64, 175)"} />
-                  チーム一覧
-                </button>
-                <button
-                  className="flex h-auto items-center"
-                  // eslint-disable-next-line arrow-body-style
-                  onClick={() => handleRouter("/NewProject")}
-                >
-                  <NewList color={"rgba(30, 64, 175)"} />
-                  新規チーム作成
-                </button>
-              </div>
-              <div className="h-auto items-center">
-                <LogoutComponent
-                  icon={<LogoutIcon color={"rgba(30, 64, 175)"} />}
-                >
-                  ログアウト
-                </LogoutComponent>
-              </div>
-            </header>
+              <ListIcon color={"rgba(30, 64, 175)"} />
+              チーム一覧
+            </button>
+
+            <button
+              className="flex h-auto items-center"
+              // eslint-disable-next-line arrow-body-style
+              onClick={() => handleRouter("/NewProject")}
+            >
+              <NewList color={"rgba(30, 64, 175)"} />
+              新規チーム作成
+            </button>
           </div>
-        </div>
-      )}
+          <div className="h-auto items-center">
+            <LogoutComponent icon={<LogoutIcon color={"rgba(30, 64, 175)"} />}>
+              ログアウト
+            </LogoutComponent>
+          </div>
+        </header>
+      </div>
     </>
   );
 };
---
+++ src/components/Menu.tsx
@@ -0,0 +1,73 @@
+import { useDisclosure } from "@chakra-ui/hooks";
+import { ListIcon, LogoutIcon, NewList } from "./Atom/Icon";
+import { useRouter } from "next/dist/client/router";
+
+import {
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+} from "@chakra-ui/modal";
+import { LogoutComponent } from "./Atom/LogoutLink";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+export const Menu = () => {
+  const router = useRouter();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleOnOpen = () => {
+    onOpen();
+  };
+
+  const handleOnClose = () => {
+    onClose();
+  };
+  const handleRouter = (url: string) => {
+    router.push(url);
+  };
+
+  return (
+    <>
+      <button className="sm:invisible top-0 fixed" onClick={handleOnOpen}>
+        <HamburgerIcon width="45px" height="45px" />
+      </button>
+
+      <Drawer placement="left" onClose={handleOnClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
+          <DrawerBody>
+            <div className="flex flex-col justify-between h-fu">
+              <div className="flex flex-col ">
+                <button
+                  className="flex h-20 items-center"
+                  // eslint-disable-next-line arrow-body-style
+                  onClick={() => handleRouter("/NewProject")}
+                >
+                  <ListIcon color={"rgba(30, 64, 175)"} />
+                  チーム一覧
+                </button>
+                <button
+                  className="flex h-20 items-center"
+                  // eslint-disable-next-line arrow-body-style
+                  onClick={() => handleRouter("/Project.page")}
+                >
+                  <NewList color={"rgba(30, 64, 175)"} />
+                  新規チーム作成
+                </button>
+              </div>
+              <div className=" flex h-24 items-center ">
+                <LogoutComponent
+                  icon={<LogoutIcon color={"rgba(30, 64, 175)"} />}
+                >
+                  ログアウト
+                </LogoutComponent>
+              </div>
+            </div>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
