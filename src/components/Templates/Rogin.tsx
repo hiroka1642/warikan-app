88de047eedc61dad37a240ae7c9d88e7e3f79458
katan4722 <uver7770422@gmail.com>
2022-01-16 22:17:38 +0900

    💄 ログインページのスタイル変更

--- src/components/Templates/Rogin.tsx
+++ src/components/Templates/Rogin.tsx
@@ -1,39 +1,21 @@
-import { Box } from "@chakra-ui/react";
 import { Auth } from "@supabase/ui";
-import { client } from "../../libs/supabase";
-import { extendTheme } from "@chakra-ui/react";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
+import { client } from "src/libs/supabase";
 
 export const Rogin: React.VFC = () => {
-  const breakpoints = createBreakpoints({
-    sm: "320px",
-    md: "600px",
-    lg: "960px",
-    xl: "1200px",
-  });
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const theme = extendTheme({ breakpoints });
-
   return (
     <>
-      <Box
-        backgroundImage={{
-          base: ".//mobileBackgroundImage.jpg",
-          md: ".//webBackgroundImage.jpg",
-        }}
-        backgroundSize="cover"
-      >
-        <div className="flex justify-center flex-col h-screen">
-          <h1 className="text-4xl text-center py-20 sm:text-6xl">
+      <div className="flex flex-row items-center h-screen">
+        <div className=" sm:w-2/5 h-full invisible sm:visible"></div>
+        <div className="w-full bg-indigo-600 flex flex-col sm:w-4/5 h-[130%] sm:rounded-l-full">
+          <h1 className="text-4xl mt-64 py-20 text-center sm:text-4xl lg:text-6xl z-50  text-white">
             みんなでわりかん。
           </h1>
-          <div>
-            <div className=" max-w-md m-auto">
-              <Auth supabaseClient={client} providers={["google"]} />
-            </div>
+          <div className="mx-auto sm:max-w-screen-sm w-5/6 z-50">
+            <Auth supabaseClient={client} providers={["google"]} />
           </div>
         </div>
-      </Box>
+      </div>
     </>
   );
 };
