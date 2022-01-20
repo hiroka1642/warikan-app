86f41fb8cb5968bb88cc2a7ed4d319b2d50dda1a
katan4722 <uver7770422@gmail.com>
2022-01-21 01:17:33 +0900

    💄

--- src/components/Templates/Rogin.tsx
+++ src/components/Templates/Rogin.tsx
@@ -5,13 +5,12 @@ export const Rogin: React.VFC = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   return (
     <>
-      <div className="flex flex-row items-center h-screen">
-        <div className=" sm:w-2/5 h-full invisible sm:visible"></div>
-        <div className="w-full bg-indigo-500 flex flex-col sm:w-4/5 h-[130%] sm:rounded-l-full">
-          <h1 className="text-4xl mt-64 py-20 text-center sm:text-4xl lg:text-6xl z-50  text-white">
+      <div className="flex flex-row items-center h-screen bg-indigo-500 ">
+        <div className="w-full bg-white flex p-10 flex-col ">
+          <h1 className="text-4xl  py-16 text-center sm:text-4xl lg:text-6xl z-50  text-indigo-500">
             みんなでわりかん。
           </h1>
-          <div className="mx-auto sm:max-w-screen-sm w-5/6 z-50">
+          <div className="mx-auto px-8  sm:max-w-screen-sm w-5/6 z-50">
             <Auth supabaseClient={client} providers={["google"]} />
           </div>
         </div>
